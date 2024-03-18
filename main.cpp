1042a9825d41ee1a561739ebba094f1987861b37
dezsokee <dezsoszabolcs46@gmail.com>
2024-03-18 19:33:16 +0200

    prod function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+int prod(int a, int b)
+{
+    return a * b;
+}
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout<<"hello, world"<<endl;
+    cout<<"hello, world1"<<endl;
+    cout<<prod(2, 3)<<endl;
     return 0;
 }
